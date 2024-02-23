b29c0b5453adbd90d7b55d57ad69b35b333b825b
wu-lezheng <wulezheng00@gmail.com>
2024-02-23 17:38:48 +0800

    Add the customisable feature section in the project page

--- src/components/product-feature/ProductFeature.jsx
+++ src/components/product-feature/ProductFeature.jsx
@@ -1,11 +1,28 @@
+import Image from 'next/image';
+
 import styles from './ProductFeature.module.css'
 
 function ProductFeature() {
+
     return (
-        <div className={styles.container}>
-            <h2>Feature title</h2>
-            <p>Feature description. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde magni iure ratione nesciunt provident alias eaque fuga nemo est. Consectetur quasi incidunt obcaecati quibusdam perferendis iste voluptas eligendi vel recusandae.</p>
-        </div>
+        <>
+            <div className={styles.containerSplit}>
+                <h2 className={styles.featureTitle}>Feature title </h2>
+                <p className={styles.featureDescription}>Feature description. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde magni iure ratione nesciunt provident alias eaque fuga nemo est. Consectetur quasi incidunt obcaecati quibusdam perferendis iste voluptas eligendi vel recusandae.</p>
+            </div>
+
+            <div className={styles.containerStack}>
+                <h2 className={styles.featureTitle}>Feature title</h2>
+                <p className={styles.featureDescription}>Feature description. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde magni iure ratione nesciunt provident alias eaque fuga nemo est. Consectetur quasi incidunt obcaecati quibusdam perferendis iste voluptas eligendi vel recusandae.</p>
+            </div>
+
+            <div className={styles.containerStack}>
+                <Image src="/products/features/project-management-mockup-computer.png" alt='' width={200} height={ }></Image>
+                <h2 className={styles.featureTitle}>Feature title</h2>
+                <p className={styles.featureDescription}>Feature description. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde magni iure ratione nesciunt provident alias eaque fuga nemo est. Consectetur quasi incidunt obcaecati quibusdam perferendis iste voluptas eligendi vel recusandae.</p>
+            </div>
+
+        </>
     );
 }
 
