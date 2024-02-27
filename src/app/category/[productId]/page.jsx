7339a18256ada2ab5c1d6edc5373c65cb7a7d691
wu-lezheng <wulezheng00@gmail.com>
2024-02-27 15:52:45 +0800

    Add responsive product feature layouts

--- src/app/category/[productId]/page.jsx
+++ src/app/category/[productId]/page.jsx
@@ -1,13 +1,34 @@
+"use client";
+
+import { useState } from 'react';
+import ProductGallery from '@/components/product-gallery/ProductGallery';
 import ProductFeature from "@/components/product-feature/ProductFeature";
 
 import styles from "./page.module.css"
 
+
 function ProductPage() {
+    const [selectedFeatures, setSelectedFeatures] = useState([]);
+
+    const handleButtonClick = (styleName, featureImage) => {
+        setSelectedFeatures(prevSelectedFeatures => [...prevSelectedFeatures, { styleName, featureImage }]);
+    };
+
     return (
         <div className={styles.container}>
-            <ProductFeature></ProductFeature>
+
+            <ProductGallery></ProductGallery>
+
+            <button onClick={() => handleButtonClick("containerSplit")}>Show Container Split</button>
+            <button onClick={() => handleButtonClick("containerStack")}>Show Container Stack</button>
+            <button onClick={() => handleButtonClick("containerImageSplit", "/products/features/project-management-mockup-phone.png")}>Show Container Image Split</button>
+            <button onClick={() => handleButtonClick("containerImageStack", "/products/features/project-management-mockup-computer.png")}>Show Container Image Stack</button>
+
+            {selectedFeatures.map((feature, index) => (
+                <ProductFeature key={index} styleName={feature.styleName} featureImage={feature.featureImage} />
+            ))}
         </div>
     );
 }
 
-export default ProductPage
+export default ProductPage;
--- src/components/product-feature/ProductFeature.jsx
+++ src/components/product-feature/ProductFeature.jsx
@@ -1,29 +1,70 @@
+"use client";
+
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
+import styles from './ProductFeature.module.css';
 
-import styles from './ProductFeature.module.css'
+function ProductFeature({ styleName, featureImage }) {
 
-function ProductFeature() {
+    const [containerClass, setContainerClass] = useState(styleName);
+
+    useEffect(() => {
+        const handleResize = () => {
+            // Check window size and update containerClass accordingly
+            if (window.innerWidth < 900) {
+                setContainerClass(styleName.replace('Split', 'Stack'));
+            } else {
+                setContainerClass(styleName);
+            }
+        };
+
+        // Add event listener for window resize
+        window.addEventListener('resize', handleResize);
+
+        // Remove event listener on component unmount
+        return () => window.removeEventListener('resize', handleResize);
+    }, [styleName]);
+
+    let containerContent;
+
+    if (containerClass === 'containerImageSplit') {
+        containerContent = (
+            <>
+                <div style={{ flex: "1", position: "relative" }}>
+                    <Image src={featureImage} alt='' width="3840" height="2160" sizes='100vw' quality={100} className={styles.featureImage} priority={true}></Image>
+                </div>
+                <div style={{ flex: "1" }}>
+                    <h2 className={styles.featureTitle}>Feature title</h2>
+                    <p className={styles.featureDescription}>Feature description. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde magni iure ratione nesciunt provident alias eaque fuga nemo est. Consectetur quasi incidunt obcaecati quibusdam perferendis iste voluptas eligendi vel recusandae.</p>
+                </div>
+            </>
+        );
+    } else if (containerClass === 'containerImageStack') {
+        containerContent = (
+            <>
+                <div>
+                    <h2 className={styles.featureTitle}>Feature title</h2>
+                    <p className={styles.featureDescription}>Feature description. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde magni iure ratione nesciunt provident alias eaque fuga nemo est. Consectetur quasi incidunt obcaecati quibusdam perferendis iste voluptas eligendi vel recusandae.</p>
+                </div>
+                <div style={{ textAlign: 'center', position: "relative" }}>
+                    <Image src={featureImage} alt='' width="3840" height="2160" sizes='100vw' quality={100} className={styles.featureImage} priority={true}></Image>
+                </div>
+            </>
+        );
+    } else {
+        containerContent = (
+            <>
+                <h2 className={styles.featureTitle}>Feature title</h2>
+                <p className={styles.featureDescription}>Feature description. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde magni iure ratione nesciunt provident alias eaque fuga nemo est. Consectetur quasi incidunt obcaecati quibusdam perferendis iste voluptas eligendi vel recusandae.</p>
+            </>
+        );
+    }
 
     return (
-        <>
-            <div className={styles.containerSplit}>
-                <h2 className={styles.featureTitle}>Feature title </h2>
-                <p className={styles.featureDescription}>Feature description. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde magni iure ratione nesciunt provident alias eaque fuga nemo est. Consectetur quasi incidunt obcaecati quibusdam perferendis iste voluptas eligendi vel recusandae.</p>
-            </div>
-
-            <div className={styles.containerStack}>
-                <h2 className={styles.featureTitle}>Feature title</h2>
-                <p className={styles.featureDescription}>Feature description. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde magni iure ratione nesciunt provident alias eaque fuga nemo est. Consectetur quasi incidunt obcaecati quibusdam perferendis iste voluptas eligendi vel recusandae.</p>
-            </div>
-
-            <div className={styles.containerStack}>
-                <Image src="/products/features/project-management-mockup-computer.png" alt='' width={200} height={ }></Image>
-                <h2 className={styles.featureTitle}>Feature title</h2>
-                <p className={styles.featureDescription}>Feature description. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde magni iure ratione nesciunt provident alias eaque fuga nemo est. Consectetur quasi incidunt obcaecati quibusdam perferendis iste voluptas eligendi vel recusandae.</p>
-            </div>
-
-        </>
+        <div className={styles[containerClass]}>
+            {containerContent}
+        </div>
     );
 }
 
-export default ProductFeature
+export default ProductFeature;
